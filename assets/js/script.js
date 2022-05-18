1fd9de8a675a9ee0d1906c88630e69fffba90bac
Laazully <mahieuxclaire@orange.fr>
2022-05-18 14:26:37 +0200

    à propos texte center

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,39 @@
+// var randomElements = document.querySelectorAll('.random') 
+// var divRandom = document.querySelector('.divRandom') 
+// // console.log(randomElements);
 
+
+// // au chargement de la page
+// window.addEventListener('onload', () => {
+    
+//     // for (element of elements){
+    
+//     // }
+    
+    
+//     $(document).ready(function(){
+//         randomElements;
+//     });
+    
+//     // fonction de random
+//     function makeNewPosition(){
+        
+//         // trouver la position x et y de la divRandom
+//         var h = $(window).height() - 50; 
+//         var w = $(window).width() - 50;
+//         // les éléments de la divRandom doivent avoir un mouvement random
+//         var nh = Math.floor(Math.random() * h);
+//         var nw = Math.floor(Math.random() * w);
+        
+//         return [nh,nw];    
+        
+//     }
+
+//     function animateDiv(myclass){
+//         var newq = makeNewPosition();
+//         $(myclass).animate({ top: newq[0], left: newq[1] }, 1000,   function(){
+//         animateDiv(myclass);        
+//         });
+        
+//     };
+// })
