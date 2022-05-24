fa20e1c9578d4b4f3dab8027fac4feb9a1c61079
Laazully <ciel.mon.hobbit@gmail.com>
2022-05-24 16:33:51 +0200

    test commit apres formatage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+
+// Tests pour déplacer les icones dans la div
+
 // var randomElements = document.querySelectorAll('.random') 
 // var divRandom = document.querySelector('.divRandom') 
 // // console.log(randomElements);
@@ -36,4 +39,43 @@
 //         });
         
 //     };
+// })
+
+
+
+// tests pour au survol dip et xp change image
+
+
+
+// var img1 = document.querySelector('#img1');
+// var img2 = document.querySelector('#img2');
+
+// img1.addEventListener('mouseover', function() {
+//     img1.style.display = 'none';
+//     img2.style.display = 'block';
+// })
+// img2.addEventListener('mouseout', function() {
+//     img1.style.display = 'block';
+//     img2.style.display = 'none';
+// })
+
+// Exercice 2  - réécriture du src
+// var img6 = document.querySelector('#img6');
+
+// img6.addEventListener('mouseover', function() {
+//     img6.src = 'assets/img/img2.png';
+// })
+
+// img6.addEventListener('mouseout', function() {
+//     img6.src = 'assets/img/img1.jpg';
+// })
+
+// Exercice 2  - opacity
+// var img4 = document.querySelector('#img4');
+
+// img4.addEventListener('mouseover', function() {
+//     img4.style.opacity = 0;
+// })
+// img4.addEventListener('mouseout', function() {
+//     img4.style.opacity = 1;
 // })
