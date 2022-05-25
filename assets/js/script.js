bab7e13b6832b8f911af06f42abc9aa103b24d34
Laazully <ciel.mon.hobbit@gmail.com>
2022-05-25 21:49:15 +0200

    accueil responsive ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,29 @@
 
+// &&&&&&&&&&&&&&&&&&&&&&&&& Accueil mode phone menu burger &&&&&&&&&&&&&&&&&&&&&&&&&
+
+
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+    if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+    } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+    }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+
+
 // Tests pour déplacer les icones dans la div
 
 // var randomElements = document.querySelectorAll('.random') 
@@ -79,3 +104,5 @@
 // img4.addEventListener('mouseout', function() {
 //     img4.style.opacity = 1;
 // })
+
+
