3fba2d51c5ed1dc2bd7ab1b5c447d74a667d0030
Laazully <ciel.mon.hobbit@gmail.com>
2022-06-03 16:34:38 +0200

    pb JS ok

---
+++ assets/js/contact.js
@@ -0,0 +1,9 @@
+// MODALE FORMULAIRE KO
+
+var validContact = document.querySelector('.mail')
+
+console.log('validContact')
+
+validContact.addEventListener('click', function() {
+    alert('Désolée, cette fonction n \'est pas encore disponible... Mais mes coordonnées sont notées sur mon CV PDF téléchargeable dans cette section.')
+})
---
+++ assets/js/diplomes.js
@@ -0,0 +1,126 @@
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&& APPARITION IMAGES DIPLOMES XP &&&&&&&&&&&&&&&&&&&&
+
+
+
+//réécriture du src
+var divIn = document.querySelector('.divIn');
+// console.log(divIn)
+
+var dateOne = document.querySelector('.dateDip1');
+var dateTwo = document.querySelector('.dateDip2');
+var dateTree = document.querySelector('.dateDip3');
+var dateFour = document.querySelector('.dateDip4');
+var dateFive = document.querySelector('.dateXp10');
+var dateSix = document.querySelector('.datesXp6');
+var dateSeven = document.querySelector('.datesXp7');
+var dateEight = document.querySelector('.datesXp8');
+var dateNine = document.querySelector('.datesXp9');
+var imgOne = document.querySelector('.imgOne');
+var imgTwo = document.querySelector('.imgTwo');
+var imgTree = document.querySelector('.imgTree');
+var imgFour = document.querySelector('.imgFour');
+var imgFive = document.querySelector('.imgFive');
+var imgSix = document.querySelector('.imgSix');
+var imgSeven = document.querySelector('.imgSeven');
+var imgEight = document.querySelector('.imgEight');
+var imgNine = document.querySelector('.imgNine');
+
+
+
+
+// LA MANU
+dateSix.addEventListener('click', function() {
+    setTimeout(function() {
+        imgOne.style.display = 'block';
+        setTimeout(function(){
+            imgOne.style.display = 'none';
+        },3000)
+    },200);
+    
+})
+
+// COMDATA
+dateSeven.addEventListener('click', function() {
+    setTimeout(function() {
+        imgTwo.style.display = 'block';
+        setTimeout(function(){
+            imgTwo.style.display = 'none';
+        },3000)
+    },200);
+})
+
+
+// LA NEUVILLE
+dateEight.addEventListener('click', function() {
+    setTimeout(function() {
+        imgTree.style.display = 'block';
+        setTimeout(function(){
+            imgTree.style.display = 'none';
+        },3000)
+    },200);
+})
+
+
+// FAMILO
+dateNine.addEventListener('click', function() {
+    setTimeout(function() {
+        imgFour.style.display = 'block';
+        setTimeout(function(){
+            imgFour.style.display = 'none';
+        },3000)
+    },200);
+})
+
+
+// MAURICE
+dateFive.addEventListener('click', function() {
+    setTimeout(function() {
+        imgNine.style.display = 'block';
+        setTimeout(function(){
+            imgNine.style.display = 'none';
+        },3000)
+    },200);
+})
+
+
+// WEBDESIGN
+dateOne.addEventListener('click', function() {
+    setTimeout(function() {
+        imgFive.style.display = 'block';
+        setTimeout(function(){
+            imgFive.style.display = 'none';
+        },3000)
+    },200);
+})
+
+
+// PCIE
+dateTwo.addEventListener('click', function() {
+    setTimeout(function() {
+        imgSix.style.display = 'block';
+        setTimeout(function(){
+            imgSix.style.display = 'none';
+        },3000)
+    },200);
+})
+
+
+// MASTER
+dateTree.addEventListener('click', function() {
+    setTimeout(function() {
+        imgSeven.style.display = 'block';
+        setTimeout(function(){
+            imgSeven.style.display = 'none';
+        },3000)
+    },200);
+})
+
+// BAC
+dateFour.addEventListener('click', function() {
+    setTimeout(function() {
+        imgEight.style.display = 'block';
+        setTimeout(function(){
+            imgEight.style.display = 'none';
+        },3000)
+    },200);
+})
