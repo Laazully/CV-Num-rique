4666d5d4a673ff1f3ebaf3d9e17504bd669ad6fd
Laazully <ciel.mon.hobbit@gmail.com>
2022-06-03 10:31:18 +0200

    Modifs hover a et padding boutons

--- assets/js/modale.js
+++
@@ -1,5 +0,0 @@
-// var validContact = document.querySelector('.mail')
-
-// validContact.addEventListener('click', function() {
-//     alert('Désolée, cette fonction n \'est pas encore disponible... Mais mes coordonnées sont notées sur mon CV PDF téléchargeable dans cette section.')
-// })
--- assets/js/script.js
+++ assets/js/script.js
@@ -150,3 +150,8 @@ dateFour.addEventListener('click', function() {
     },200);
 })
 
+var validContact = document.querySelector('.mail')
+
+validContact.addEventListener('click', function() {
+    alert('Désolée, cette fonction n \'est pas encore disponible... Mais mes coordonnées sont notées sur mon CV PDF téléchargeable dans cette section.')
+})
