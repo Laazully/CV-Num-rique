8c7bd613e55b4cd5115b67003fef65c7ecb8a432
Laazully <ciel.mon.hobbit@gmail.com>
2022-07-31 17:34:49 +0200

    modifs AE et VS code + tailles pictos

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -5,5 +5,5 @@ var validContact = document.querySelector('.mail')
 console.log('validContact')
 
 validContact.addEventListener('click', function() {
-    alert('Désolée, cette fonction n \'est pas encore disponible... Mais mes coordonnées sont notées sur mon CV PDF téléchargeable dans cette section.')
+    alert('Désolée, cette fonction n \'est pas encore disponible... Mais mes coordonnées sont notées sur mon CV PDF téléchargeable dans cette section. Ou vous pouvez me contacter via mon portfolio.')
 })
